e7ca424a4949382bd7aec89b186a4550f7f226af
Ryankwon03 <ryankwon@umich.edu>
2024-04-12 18:27:56 -0400

    Update ai.py

--- modules/ai.py
+++ modules/ai.py
@@ -1,43 +1,51 @@
 # Modules
-from app import GOOGLE_API_KEY
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import PIL.Image
 import google.generativeai as genai
 
 # API Key 는 GOOGLE_API_KEY 로 쓰시면 됩니다
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
 
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-
-
-#then run the function
-
-#model.generate_content([text, image]) --> for gemini-pro-vision
 
 
 #temperature = randomness of result
 #token limit
-
-
-#file API = audio file --> only lasts for 2 days, and upload whatever thing we want and bring that up later
-
-
-#system instructions
+#system instructions --> you are fucking albert einstein who can solve every fucking problem in the world (ㅈ나 빨아주기)
 #embedding
 
-def gemini_use(input_text = "what's ur name", input_image = None, input_file_API = None):
+#박이안 참고용:
+    #gemini_use(input_text = text, input_image = image, input_file_API = fileAPI) --> output = response
+
+def get_text(response):
+    return response.text
+
+#get_index
+#get_token_count
+#get_safety_rating_warnings
+#get_finish_reason --> 1 = completed
+
+
+def gemini_use(input_text = "give me 10 random numbers", input_image = None, input_file_API = None):
     if(input_file_API != None):
         return ''
     elif(input_image != None): #image exists --> use vision pro
         return ''
     else: #제일 간단한 text-to-text
-        return gemini_text_input(input_file_API)
+        return gemini_text_input(input_text)
     
     return ''
             
   
 
 def gemini_text_input(input_text):
-    genai.GenerativeModel('gemini-pro')
-    return(genai.generate_text(input_text))
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input_text)
+    return response
 
 
+print(get_text(gemini_use()))
