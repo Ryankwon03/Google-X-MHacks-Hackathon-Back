fc1577aec43dab6e0d29ec3bbca9159a8a439d84
Ryankwon03 <ryankwon@umich.edu>
2024-04-13 17:22:14 -0400

    update

--- modules/ai.py
+++ modules/ai.py
@@ -129,7 +129,7 @@ def create_chroma_db(documents, name):
             documents = [d],
             ids = [str(i)]
         )
-        return db
+    return db
     
 #use this by doing db = create_chroma_db(documents, "googlecarsdatabase")
 
@@ -137,7 +137,20 @@ def get_relavant_passage(query, db): #KNN algorithm 써서 한다
     passage = db.query(query_texts = [query], n_results = 1)['documents'][0][0]
     return passage
 
+def make_prompt(query, relevant_passage):
+    escaped = relevant_passage.replace("'", "").replace('"', "").replace("\n", " ")
+    prompt = ("""You are a helpful and informative bot that answers questions using text from the reference passage included below. \
+    Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+    However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+    strike a friendly and converstional tone. \
+    If the passage is irrelevant to the answer, you may ignore it.
+    QUESTION: '{query}'
+    PASSAGE: '{relevant_passage}'
 
+        ANSWER:
+    """).format(query=query, relevant_passage=escaped)
+
+    return prompt
 
 
 #DEMO RUN OF THE ABOVE
@@ -151,9 +164,16 @@ def get_relavant_passage(query, db): #KNN algorithm 써서 한다
 DOCUMENT1 = "Operating the Climate Control System  Your Googlecar has a climate control system that allows you to adjust the temperature and airflow in the car. To operate the climate control system, use the buttons and knobs located on the center console.  Temperature: The temperature knob controls the temperature inside the car. Turn the knob clockwise to increase the temperature or counterclockwise to decrease the temperature. Airflow: The airflow knob controls the amount of airflow inside the car. Turn the knob clockwise to increase the airflow or counterclockwise to decrease the airflow. Fan speed: The fan speed knob controls the speed of the fan. Turn the knob clockwise to increase the fan speed or counterclockwise to decrease the fan speed. Mode: The mode button allows you to select the desired mode. The available modes are: Auto: The car will automatically adjust the temperature and airflow to maintain a comfortable level. Cool: The car will blow cool air into the car. Heat: The car will blow warm air into the car. Defrost: The car will blow warm air onto the windshield to defrost it."
 DOCUMENT2 = "Your Googlecar has a large touchscreen display that provides access to a variety of features, including navigation, entertainment, and climate control. To use the touchscreen display, simply touch the desired icon.  For example, you can touch the \"Navigation\" icon to get directions to your destination or touch the \"Music\" icon to play your favorite songs."
 DOCUMENT3 = "Shifting Gears Your Googlecar has an automatic transmission. To shift gears, simply move the shift lever to the desired position.  Park: This position is used when you are parked. The wheels are locked and the car cannot move. Reverse: This position is used to back up. Neutral: This position is used when you are stopped at a light or in traffic. The car is not in gear and will not move unless you press the gas pedal. Drive: This position is used to drive forward. Low: This position is used for driving in snow or other slippery conditions."
+DOCUMENT4 = "A American girl named Seoin is pretty"
+DOCUMENT5 = "A Korean guy named sungmo is pretty"
 
-documents = [DOCUMENT1, DOCUMENT2, DOCUMENT3]
+documents = [DOCUMENT1, DOCUMENT2, DOCUMENT3, DOCUMENT4, DOCUMENT5] #여기에 들어가는 query 는 유저가 넣은 query
+#박이안이 이렇게 추가해서 쓰는거
 db = create_chroma_db(documents, "googlecarsdatabase")
-passage = get_relavant_passage("touch screen features", db)
-
+passage = get_relavant_passage("touchscreen", db)
 print(passage)
+
+# query = "How do you use the touchscreen in the Google car?"
+# prompt = make_prompt(query, passage)
+# print(prompt)
+
